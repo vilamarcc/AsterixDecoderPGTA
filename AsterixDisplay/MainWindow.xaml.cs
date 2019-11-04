764e093676b2ed95bb06c761cbbd100ef1e62d6e
vilamarcc <vilamarcc@gmail.com>
2019-11-04 12:53:36 +0100

    WPF implementation

---
+++ AsterixDisplay/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AsterixDecoder;
+
+namespace AsterixDisplay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+
+        string fichero;
+        DataTable dataCAT20;
+        DataTable dataCAT10;
+        DataTable dataCAT21;
+        DataTable dataCATsearch;
+        Fichero f;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void cargarbut_Click(object sender, RoutedEventArgs e)
+        {
+            ofd.Filter = "AST |*.ast";
+            var progress = new Progress<int>(value => progressbar1.Value = value);
+            progressbar1.Visibility = Visibility.Visible;
+
+            if (ofd.ShowDialog() == true)
+            {
+                await Task.Run(() =>
+                {
+                    this.fichero = ofd.FileName;
+                    f = new Fichero(this.fichero);
+                    ((IProgress<int>)progress).Report(10);
+                    f.Read();
+                    ((IProgress<int>)progress).Report(60);
+                    MessageBox.Show("Archivo cargado");
+                    ((IProgress<int>)progress).Report(75);
+                    dataCAT20 = f.getTablaCAT20();
+
+                    
+                    ((IProgress<int>)progress).Report(100);
+                    
+                });
+                fillgridwithdata(dataCAT20);
+                progressbar1.Visibility = Visibility.Hidden;
+            }
+        }
+
+        public void fillgridwithdata(DataTable data)
+        {
+            gridCAT.ItemsSource = data.DefaultView;
+            gridCAT.Items.Refresh();
+        }
+
+        void ClikDataGrid(object sender, RoutedEventArgs e)
+        {
+            DataGridCell cell = (DataGridCell)sender;
+            int c = cell.Column.DisplayIndex;
+            DataGridRow r2 = DataGridRow.GetRowContainingElement(cell);
+            int f = r2.GetIndex();
+            if (c == 8) //Track status
+            {
+                MessageBox.Show("Track status shit aqui");
+            }
+            if (c == 20) //Pos Accuracy
+            {
+                MessageBox.Show("Doppler shit aqui");
+            }
+            if (c == 22) //Mode S MB Data
+            {
+                MessageBox.Show("Data Mode S aqui");
+            }
+
+            filldataexpandedCAT20(f);
+        }
+        private void filldataexpandedCAT20(int index)
+        {
+            DataTable expanded = new DataTable();
+            CAT20 cat20exp = f.getCAT20(index);
+
+            expanded.Columns.Add(new DataColumn());
+            expanded.Columns.Add(new DataColumn());
+
+            expanded.Rows.Add("Package #",index + 1);
+            expanded.Rows.Add("FSPEC",cat20exp.FSPEC);
+            try { expanded.Rows.Add("Data Source ID", "SAC: " + cat20exp.SAC + " SIC: " + cat20exp.SIC); } catch { }
+            try { expanded.Rows.Add("Target Report", cat20exp.TargetReport); } catch { } //Falta desarrollar esto --> funcion en la clase para descifrar tipo de msg
+            try { expanded.Rows.Add("Time of Day (UTC)", cat20exp.TOD); } catch { }
+            try { expanded.Rows.Add("Position in WSG-84", "[" + cat20exp.LatWSG.ToString() + "," + cat20exp.LonWSG.ToString() + "]"); } catch { }
+            try { expanded.Rows.Add("Position (X, Y)", "[" + cat20exp.X.ToString() + "," + cat20exp.Y.ToString() + "] m"); } catch { }
+            try { expanded.Rows.Add("Track Number", cat20exp.TrackNum); } catch { }
+            try { expanded.Rows.Add("Track Status", cat20exp.CDM); } catch { }
+            try { expanded.Rows.Add("Mode-3/A Code", cat20exp.Mode3A.ToString()); } catch { }
+            try { expanded.Rows.Add("Track Velocity (Vx, Vy)", "[" + cat20exp.Vy.ToString() + "," + cat20exp.Vy.ToString() + "] m/s"); } catch { }
+            try { expanded.Rows.Add("Flight Level", cat20exp.FL[2]); } catch { }
+            try { expanded.Rows.Add("Mode-C Code", cat20exp.ModeC.ToString()); } catch { }
+            try { expanded.Rows.Add("Target Address", cat20exp.TargetAddress.ToString()); } catch { }
+            try { expanded.Rows.Add("Target ID", cat20exp.TargetID[1]);} catch { }
+            try { expanded.Rows.Add("Measured Height", cat20exp.MeasuredHeight.ToString()); } catch { }
+            try { expanded.Rows.Add("Geometric Height", cat20exp.geoHeight.ToString()); } catch { }
+            try { expanded.Rows.Add("Calculated Accel", cat20exp.calcAccel.ToString()); } catch { }
+            try { expanded.Rows.Add("Vehicle Fleet ID", cat20exp.VehicleFleetID.ToString()); } catch { }
+            try { expanded.Rows.Add("Pre-Programmed Message", cat20exp.PPMsg.ToString()); } catch { }
+            try { expanded.Rows.Add("Position Accuracy:", "Falta desarrollar"); } catch { }
+            try { expanded.Rows.Add("Receivers", cat20exp.Receivers.ToString()); } catch { }
+            try { expanded.Rows.Add("Mode S MB Data", cat20exp.ModeSData.ToString()); } catch { }
+
+            dataexpanded.ItemsSource = expanded.DefaultView;
+            dataexpanded.Items.Refresh();
+
+        }
+        private void searchbut_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int package = Convert.ToInt32(searchbox.Text) - 1;
+                CAT20 cat20search = f.getCAT20(package);
+
+                dataCATsearch = f.getTablaCAT20Indv(cat20search, package);
+
+                gridCAT.ItemsSource = dataCATsearch.DefaultView;
+                gridCAT.Items.Refresh();
+
+                filldataexpandedCAT20(package);
+            }
+            catch
+            {
+                MessageBox.Show("Packete no dispoible, comprueba el #");
+            }
+        }
+
+        private void clearsearchbut_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                gridCAT.ItemsSource = dataCAT20.DefaultView;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SimulacionPanel sim = new SimulacionPanel();
+            sim.ShowDialog();
+        }
+    }
+}
---
+++ AsterixDisplay/SimulacionPanel.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GMap.NET.WindowsPresentation;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using AsterixDecoder;
+using System.Drawing;
+using System.Resources;
+
+namespace AsterixDisplay
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class SimulacionPanel : Window
+    {
+        double[] MLATcoords = new double[2];
+        
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer(); //Para el tick del timer
+        public SimulacionPanel()
+        {
+            InitializeComponent();
+            MLATcoords[0] = 41.297063;
+            MLATcoords[1] = 2.078447;
+        }
+
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            // choose your provider here
+            mapView.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            mapView.MinZoom = 14;
+            mapView.MaxZoom = 16;
+            // whole world zoom
+            mapView.Zoom = 2;
+            // lets the map use the mousewheel to zoom
+            mapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            // lets the user drag the map
+            mapView.CanDragMap = true;
+            // lets the user drag the map with the left mouse button
+            mapView.DragButton = MouseButton.Left;
+            mapView.Position = new GMap.NET.PointLatLng(MLATcoords[0], MLATcoords[1]);
+
+            GMap.NET.WindowsPresentation.GMapMarker marker = new GMap.NET.WindowsPresentation.GMapMarker(new PointLatLng(MLATcoords[0], MLATcoords[1]));
+            marker.Position = new PointLatLng(MLATcoords[0], MLATcoords[1]);
+            marker.Shape = new System.Windows.Controls.Image
+            {
+                Width = 15,
+                Height = 15,
+                Source = new BitmapImage(new System.Uri("pack://application:,,,/Resources/airplane.jpg"))
+                
+            };
+
+            mapView.Markers.Add(marker);
+            mapView.ReloadMap();
+        }
+    }
+}
